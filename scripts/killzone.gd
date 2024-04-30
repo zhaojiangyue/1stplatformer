e8af9e20e03067260310bfbf82aac709440451f1
zhaojiangyue <jiangyue.zh@gmail.com>
2024-04-30 18:38:37 +0800

    slow time during dying

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -4,9 +4,11 @@ extends Area2D
 
 func _on_body_entered(body):
 	print("You died!")
+	Engine.time_scale = 0.3
 	timer.start()
 	
 
 
 func _on_timer_timeout():
 	get_tree().reload_current_scene()
+	Engine.time_scale = 1.0
